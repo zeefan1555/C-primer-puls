468c49ebd478616f7ac7e7e317085c38b94ad8d5
zeefan1555 <92498230+zeefan1555@users.noreply.github.com>
2022-02-18 10:56:22 +0800

    Add files via upload

---
+++ 7.9迭代法合集代码.cpp
@@ -0,0 +1,62 @@
+//16
+#include <iostream>
+using namespace std;
+
+void countdown (int n);
+
+int mian(void)
+{
+   countdown(4);
+    return 0;
+}
+void countdown (int n){
+    cout<<n<<endl;
+    if(n > 0) countdown(n - 1);
+    cout<<n<<endl;
+}
+
+
+//17
+#include <iostream>
+
+using namespace std;
+
+const int Len = 66;
+const int Divs = 6;
+
+void subdivide(char ar[], int low, int high, int levels);
+
+int main(void)
+{
+	char ruler[Len];
+
+	for(int i = 0; i < Len; i++)
+		ruler[i] = ' ';
+
+int min = 0;
+int max = Len - 2;
+	ruler[Len-1] = '\0';
+	ruler[min] = ruler[max] = '|';
+
+	cout << ruler << endl;
+
+	for(int i = 1; i <= Divs; i++)
+	{
+		subdivide(ruler, min, max, i);
+		cout << ruler << endl;
+	}
+
+	return 0;
+}
+
+void subdivide(char ar[], int low, int high, int levels)
+{
+	if(levels == 0)
+		return;
+
+	int mid = (low + high) / 2;
+	ar[mid] = '|';
+
+	subdivide(ar, low, mid, levels-1);
+	subdivide(ar, mid, high, levels-1);
+}
